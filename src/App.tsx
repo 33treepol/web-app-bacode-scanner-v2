139026e098da6bb96d8d2cf92e33ebec1411b96e
33_treepol <t.pintook@gmail.com>
2024-09-22 19:02:49 +0700

    Xwing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from "react";
 import { Container, Typography } from "@mui/material";
-import CustomQrScanner from "./QrScanner";
+import CustomQrScanner from "./CustomQrScanner";
 
 function App() {
   const [data, setData] = React.useState<string>("Not Found");
 
-  const handleScan = (result: any) => {
-    if (result) {
-      setData(result.text);
-    } else {
-      setData("Not Found");
-    }
+  const handleScan = (result: string) => {
+    setData(result);
   };
 
-  const handleError = (err: any) => {
-    console.error(err);
+  const handleError = (error: Error) => {
+    console.error("QR Scan Error:", error);
   };
 
   return (
     <Container style={{ textAlign: "center", paddingTop: "20px" }}>
       <CustomQrScanner onScan={handleScan} onError={handleError} />
-      <Typography variant="h6">just change test: {data}</Typography>
+      <Typography variant="h6">data come: {data}</Typography>
     </Container>
   );
 }
